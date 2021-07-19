a09b9c204236a60716c7ee7e4d152fd2c11b3c93
infra-ci <contact@infrahq.com>
2021-07-19 14:37:00 -0400

    Brew formula update for infra version v0.0.12

--- infra.rb
+++ infra.rb
@@ -5,28 +5,28 @@
 class Infra < Formula
   desc "Infra"
   homepage "https://infrahq.com"
-  version "0.0.11"
+  version "0.0.12"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://releases.infrahq.com/infra/v0.0.11/infra_0.0.11_darwin_x86_64.zip"
-      sha256 "2a4fa2f23c20fe4b36cd9d624941495152408c7a82691539122aaa343740a21e"
+      url "https://releases.infrahq.com/infra/v0.0.12/infra_0.0.12_darwin_x86_64.zip"
+      sha256 "e2bc3cb5a1d1a528110da77b5daf03f7929c7d8097d3d83ddbdbc3f9f6c2bf9e"
     end
     if Hardware::CPU.arm?
-      url "https://releases.infrahq.com/infra/v0.0.11/infra_0.0.11_darwin_arm64.zip"
-      sha256 "9ba0a260a185094c8d4673184c186761fdb45977983b5e67f7ad76da2d4179cb"
+      url "https://releases.infrahq.com/infra/v0.0.12/infra_0.0.12_darwin_arm64.zip"
+      sha256 "c086de7bff3773b978952bd62137ddd800ceb943be7227bda59bc0591e00443e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://releases.infrahq.com/infra/v0.0.11/infra_0.0.11_linux_x86_64.zip"
-      sha256 "fde9bc72ed7c0729f60ed3a0c4aa3e0c5529c77807af6cadd8bf2317eb8c0366"
+      url "https://releases.infrahq.com/infra/v0.0.12/infra_0.0.12_linux_x86_64.zip"
+      sha256 "4b2546248afc27facdab299d7f7a84c198a022656d65d8a8b011aaec6bc68b2a"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://releases.infrahq.com/infra/v0.0.11/infra_0.0.11_linux_arm64.zip"
-      sha256 "e78e793b637f0a32e875f811cf4a63bfaf2a72aa9419ed0b4476918cd28c9e82"
+      url "https://releases.infrahq.com/infra/v0.0.12/infra_0.0.12_linux_arm64.zip"
+      sha256 "fe8c853450a2b893545ae173ac3b89aa56c9654db5113161bdaa0f44ec75d529"
     end
   end
 
