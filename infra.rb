f91bfdcabc7b23bb6adf603adbc53403dad6ce7d
infrahq-ci <contact@infrahq.com>
2022-11-22 00:20:19 +0000

    Homebrew tap update for infra version 0.18.0

--- infra.rb
+++ infra.rb
@@ -1,12 +1,12 @@
 class Infra < Formula
   desc "Infra"
   homepage "https://infrahq.com"
-  version "0.17.1"
+  version "0.18.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/infrahq/infra/releases/download/v0.17.1/infra_0.17.1_darwin_x86_64.zip"
-      sha256 "250d07308f805c27b3346ee2d06b3939c6942c7ff87caacdcbd7315dc8977f82"
+      url "https://github.com/infrahq/infra/releases/download/v0.18.0/infra_0.18.0_darwin_x86_64.zip"
+      sha256 "385e799149e0779bc096747ae6492790db133d144c1c16dac8713a214e78a51c"
 
       def install
         bin.install "infra"
@@ -19,8 +19,8 @@ class Infra < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/infrahq/infra/releases/download/v0.17.1/infra_0.17.1_darwin_arm64.zip"
-      sha256 "00a666444bc9e36d4dedd1c3a7f05a165f3f75eec988882588c6b673a3ca2f1c"
+      url "https://github.com/infrahq/infra/releases/download/v0.18.0/infra_0.18.0_darwin_arm64.zip"
+      sha256 "79cd6876c0e1887ce681497b29bdffbefb7969a62bb92108c072f3839c6b890b"
 
       def install
         bin.install "infra"
@@ -36,8 +36,8 @@ class Infra < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/infrahq/infra/releases/download/v0.17.1/infra_0.17.1_linux_arm64.zip"
-      sha256 "4a27b8c4267d0e3648caf2cda4cceca0c48633223ab9cec5b131819e491f8db7"
+      url "https://github.com/infrahq/infra/releases/download/v0.18.0/infra_0.18.0_linux_arm64.zip"
+      sha256 "b8f2ae1b949cc2b9c0b10e5c1f1369898d2b638704413c9fa1b24cf2b4cd256e"
 
       def install
         bin.install "infra"
@@ -50,8 +50,8 @@ class Infra < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/infrahq/infra/releases/download/v0.17.1/infra_0.17.1_linux_x86_64.zip"
-      sha256 "e162dd4647b89164408512ef2195daa16786a6d4ec69caf809e7f5f1e396fe3d"
+      url "https://github.com/infrahq/infra/releases/download/v0.18.0/infra_0.18.0_linux_x86_64.zip"
+      sha256 "eee75d0dbefc3e6794d4ae757ecfbca3f7016977ad0c4a412ad2347fd4389426"
 
       def install
         bin.install "infra"
